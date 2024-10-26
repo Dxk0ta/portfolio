2e36a43c0f33594aa3c1d3fa329038a6b599d5f3
dxk0ta <dakotacamps@gmail.com>
2024-10-25 18:30:18 -0700

    adding about me page

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './AboutMe.css';
 
 const AboutMe = () => {
   return (
-    <div className="section" id="about-me">
-      <h2>About Me</h2>
-      <p>I'm Dakota, a full-stack software developer from Grass Valley, CA.</p>
-      <p>I combine my technical skills with seven years of experience in healthcare to deliver empathetic and user-focused solutions.</p>
+    <div id="aboutme" className="aboutme">
+      <h2 className="aboutme-title">About Me</h2>
+      <p className="aboutme-description">
+        I'm Dakota, a full-stack software developer from <strong>Los Angeles, CA</strong>.
+      </p>
+      <p className="aboutme-description">
+        I combine my technical skills with <strong>seven years of experience in healthcare</strong> to deliver great attention to detail and user-focused solutions.
+      </p>
+      <p className="aboutme-quote">
+      “Empathy in action: transforming ideas into impactful solutions.”
+      </p>
     </div>
   );
 };
